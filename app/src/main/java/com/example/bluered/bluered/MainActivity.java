aef6eb12e8ce4e55a73a4a018917d0c6b0d04db5
KonradGM <34782950+DonutDonkey@users.noreply.github.com>
2018-11-23 16:22:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/bluered/bluered/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.bluered.bluered;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void swapLeft(View view) {
+        Intent intent = new Intent(this, LeftActivity.class);
+        startActivity(intent);
+    }
+
+    public void swapRight(View view) {
+        Intent intent = new Intent(this, RightActivity.class);
+        startActivity(intent);
+    }
+}
